5600b320bb71608331b0253060c3858c92b97d87
Gisele Bartolo <giselebartolo@gmail.com>
2020-09-28 18:18:18 -0300

    Modificaciones en css

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,15 @@
-<script> 
-    const input = document.querySelector('#sample1'); 
-    const icon = document.querySelector('.input-with-icon i'); 
-    icon.addEventListener('click', function() { 
-      event.preventDefault();
-      const type = (input.getAttribute('type') === 'text') ? 'password' : 'text'; 
-      input.setAttribute('type', type); 
-      }); 
- </script>
+const input = document.querySelector('#password');
+const icon = document.querySelector('.input-with-icon i');
+icon.addEventListener('click', (event) => {
+    event.preventDefault();
+    const type = (input.getAttribute('type') === 'text') ? 'password' : 'text';
+    input.setAttribute('type', type);
+}); 
+
+
+function enableButton(password) {
+let bt = document.getElementById('inicio-sesion-btn');
+  if ((password).value != '') {
+      bt.classList.add('enabled-btn');
+  }
+}
