c5a7803cfa228f0f23b0e9e5dce7180618ef3ac5
gisele89 <giselebartolo@gmail.com>
2020-10-02 14:53:10 -0300

    Modificacion de css y html en todos los archivos

--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,12 @@ icon.addEventListener('click', (event) => {
     event.preventDefault();
     const type = (input.getAttribute('type') === 'text') ? 'password' : 'text';
     input.setAttribute('type', type);
-}); 
+});
 
 
-let pass=document.getElementById(password);
-let user=document.getElementById(usuario);
+let pass = document.getElementById(password);
+let user = document.getElementById(usuario);
 let bt = document.getElementById('inicio-sesion-btn');
-  if ((pass.value != '') && (user.value!='')) {
-      bt.classList.remove('disabled-btn');
-  }
+if ((pass.value != '') && (user.value != '')) {
+    bt.classList.remove('disabled-btn');
+}
