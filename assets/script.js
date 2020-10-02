8abaa82a41299ce7750c29f109bb3bb8465c677a
gisele89 <giselebartolo@gmail.com>
2020-10-02 18:02:58 -0300

    Cambios en mobile

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ icon.addEventListener('click', (event) => {
     const type = (input.getAttribute('type') === 'text') ? 'password' : 'text';
     input.setAttribute('type', type);
 });
-
+/*
 
 let pass = document.getElementById(password);
 let user = document.getElementById(usuario);
@@ -13,3 +13,33 @@ let bt = document.getElementById('inicio-sesion-btn');
 if ((pass.value != '') && (user.value != '')) {
     bt.classList.remove('disabled-btn');
 }
+
+
+var inputs = "input[maxlength]";
+	$(document).on('keyup', "[maxlength]", function (e) {
+		var este = $(this),
+			maxlength = este.attr('maxlength'),
+			maxlengthint = parseInt(maxlength),
+			textoActual = este.val(),
+			currentCharacters = este.val().length;
+			remainingCharacters = maxlengthint - currentCharacters,
+			espan = este.prev('p').find('span');			
+			// Detectamos si es IE9 y si hemos llegado al final, convertir el -1 en 0 - bug ie9 porq. no coge directamente el atributo 'maxlength' de HTML5
+			if (document.addEventListener && !window.requestAnimationFrame) {
+				if (remainingCharacters <= -1) {
+					remainingCharacters = 0;            
+				}
+			}
+			espan.html(remainingCharacters);
+			if (!!maxlength) {
+				var texto = este.val();	
+				if (texto.length >= maxlength) {
+					este.removeClass().addClass("borderojo");
+					este.val(text.substring(0, maxlength));
+					e.preventDefault();
+				}
+				else if (texto.length < maxlength) {
+					este.removeClass().addClass("bordegris");
+				}	
+			}	
+		});*/
